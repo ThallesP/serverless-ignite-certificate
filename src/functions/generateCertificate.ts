4fb632c98774ba98e142ef81c3439de512f686fc
ThallesP <contato@thalles.me>
2021-11-26 21:31:25 -0300

    :tada: Upload project

---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,89 @@
+import chromium from "chrome-aws-lambda";
+import fs from "fs";
+import path from "path";
+import { document } from "../utils/dynamodbClient";
+import handlebars from "handlebars";
+import dayjs from "dayjs";
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async (data: ITemplate) => {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
+};
+
+export const handle = async (event) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  await document
+    .put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade,
+      },
+    })
+    .promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    id,
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    medal,
+  };
+
+  const content = await compile(data);
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ message: "Certificate created" }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
